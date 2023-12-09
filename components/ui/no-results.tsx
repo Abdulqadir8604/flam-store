c5464f4a7ca0580519be4bf09e31d081ff77de33
Abdulqadir8604 <web.aq8604@gmail.com>
2023-12-09 21:48:56 +0530

    fixex ' typo with &#39;

--- components/ui/no-results.tsx
+++ components/ui/no-results.tsx
@@ -1,7 +1,7 @@
 const NoResults = () => {
     return (
         <div className="flex items-center justify-center h-full w-full text-neutral-500">
-            <p>Sorry, we couldn't find any results for that search.</p>
+            <p>Sorry, we couldn&#39;t find any results for that search.</p>
         </div>
     )
 }
